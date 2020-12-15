947fef935d7273b871f3b1cd92ac8b3a14a074d3
Hiep Do <gao619@gmail.com>
2020-12-15 17:21:31 -0500

    add implementations for DAO

--- backend/src/main/java/com/example/demo/dao/UserCarInfoIMPL.java
+++ backend/src/main/java/com/example/demo/dao/UserCarInfoIMPL.java
@@ -1,4 +1,27 @@
 package com.example.demo.dao;
 
-public class UserCarInfoIMPL {
+import com.example.demo.entities.UserCarInfo;
+
+import java.util.List;
+
+public class UserCarInfoIMPL implements  UserCarInfoDAO {
+    @Override
+    public List<UserCarInfo> fetchAll() {
+        return null;
+    }
+
+    @Override
+    public UserCarInfo fetchById(int theId) {
+        return null;
+    }
+
+    @Override
+    public void save(UserCarInfo userCarInfo) {
+
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
 }
--- backend/src/main/java/com/example/demo/dao/UsersIMPL.java
+++ backend/src/main/java/com/example/demo/dao/UsersIMPL.java
@@ -1,4 +1,27 @@
 package com.example.demo.dao;
 
-public class UsersIMPL {
+import com.example.demo.entities.Users;
+
+import java.util.List;
+
+public class UsersIMPL implements  UsersDAO {
+    @Override
+    public List<Users> fetchAll() {
+        return null;
+    }
+
+    @Override
+    public Users fetchById(int theId) {
+        return null;
+    }
+
+    @Override
+    public void save(Users users) {
+
+    }
+
+    @Override
+    public void deleteById(int theId) {
+
+    }
 }
