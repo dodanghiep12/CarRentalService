2284133d7253110d1e52b33f18a254abbfab532e
Hiep Do <gao619@gmail.com>
2020-12-16 13:16:43 -0500

    add implementations to DAO

--- backend/src/main/java/com/example/demo/dao/CarStockIMPL.java
+++ backend/src/main/java/com/example/demo/dao/CarStockIMPL.java
@@ -1,27 +1,52 @@
 package com.example.demo.dao;
 
 import com.example.demo.entities.CarStock;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
+@Repository
 public class CarStockIMPL implements CarStockDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public CarStockIMPL(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
+    @Transactional
     public List<CarStock> fetchAll() {
-        return null;
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<CarStock> myQuery = currentSession.createQuery("from CarStock");
+        return myQuery.getResultList();
     }
 
     @Override
+    @Transactional
     public CarStock fetchById(int theId) {
-        return null;
+        Session currentSession = entityManager.unwrap(Session.class);
+        return currentSession.get(CarStock.class, theId);
     }
 
     @Override
+    @Transactional
     public void save(CarStock carStock) {
-
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(carStock);
     }
 
     @Override
+    @Transactional
     public void deleteById(int theId) {
-
+        Session currentSession = entityManager.unwrap(Session.class);
+        CarStock myCarStock = currentSession.get(CarStock.class, theId);
+        currentSession.delete(myCarStock);
     }
 }
--- backend/src/main/java/com/example/demo/dao/UsersIMPL.java
+++ backend/src/main/java/com/example/demo/dao/UsersIMPL.java
@@ -1,27 +1,52 @@
 package com.example.demo.dao;
 
 import com.example.demo.entities.Users;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
+@Repository
 public class UsersIMPL implements  UsersDAO {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public UsersIMPL(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
+    @Transactional
     public List<Users> fetchAll() {
-        return null;
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Users> myQuery = currentSession.createQuery("from Users");
+        return myQuery.getResultList();
     }
 
     @Override
+    @Transactional
     public Users fetchById(int theId) {
-        return null;
+        Session currentSession = entityManager.unwrap(Session.class);
+        return currentSession.get(Users.class, theId);
     }
 
     @Override
+    @Transactional
     public void save(Users users) {
-
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(users);
     }
 
     @Override
+    @Transactional
     public void deleteById(int theId) {
-
+        Session currentSession = entityManager.unwrap(Session.class);
+        Users myUser = currentSession.get(Users.class, theId);
+        currentSession.delete(myUser);
     }
 }
