9af5564d8e699bf281309a9785c27f7ad83306ce
Hiep Do <gao619@gmail.com>
2020-12-16 15:52:33 -0500

    add header and footer component

---
+++ frontend/src/headerandfooter/Footer Component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class FooterComponent extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                <span className="text-muted">All Rights Reserved 2020 by: Charles Saeyang, Hiep Do, and Dan Fellows</span>
+            </footer>
+        )
+    }
+}
+
+export default FooterComponent
