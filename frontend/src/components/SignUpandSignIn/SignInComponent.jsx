61e1221179d9e9be40cdd6e2591b0900e7df3577
Hiep Do <gao619@gmail.com>
2020-12-17 10:21:50 -0500

    add components to frontend react

---
+++ frontend/src/components/SignUpandSignIn/SignInComponent.jsx
@@ -0,0 +1,94 @@
+import React, { Component} from 'react';
+import { Redirect } from "react-router-dom";
+import UserDataService from '../../services/UserDataService';
+import { Formik, Form, Field } from 'formik';
+class SignInComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            password: '',
+            redirect: false,
+            id:0
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(values) {
+
+        let user = {
+            username: values.userName,
+            password: values.password
+        }
+
+        UserDataService.retrieveAllUser()
+        .then(
+            (response) => {
+                response.data.forEach((element) => {
+                    if(element.userName === user.username && element.password === user.password){
+                        this.setState({
+                            id: element.id,
+                            redirect : true
+                          });     
+
+                    } 
+
+                });
+            }
+        )        
+    }
+
+    render() {
+        let {userName, password} = this.state
+        if (this.state.redirect) {
+            //return <Redirect to={"/FlashcardSets"} />  
+            
+            return <Redirect
+            to={{
+            pathname: "/FlashcardSets",
+            state: { id: this.state.id }
+          }}
+        />
+        }
+
+        return (  
+            
+            <div className="base-container">
+                <br/>
+                <div className="header">Sign In</div>
+                <div className="content">
+                <div className="image">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/61CfipGxS-L._AC_SL1001_.jpg" alt="new"/>
+                </div>                    
+                    <Formik
+                        initialValues={{userName, password}}
+                        onSubmit={this.onSubmit}
+                        enableReinitialize={true}
+                    >
+                        {
+                            (props) => (
+                                <Form className="form">
+                                    <fieldset className="form-group">
+                                        <label>Username</label>
+                                        <Field className="form-control" type="text" name="userName" placeholder="username" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Password</label>
+                                        <Field className="form-control" type="text" name="password" placeholder="password" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <button className="btn" type="submit">Sign In</button>
+                                    </fieldset>
+                                    <br/>
+                                    <br/>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SignInComponent;
---
+++ frontend/src/components/SignUpandSignIn/SignUpComponent.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import { Formik, Form, Field } from 'formik';
+import UserDataService from '../../services/UserDataService';
+
+
+class CreateNewUserComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id: this.props.match.params.id,
+            firstName: '',
+            lastName: '',
+            userName: '',
+            password: ''
+        }
+        this.onSubmit = this.onSubmit.bind(this)
+    }
+
+    onSubmit(values) {
+        let user = {
+            userName: values.userName,
+            password: values.password
+        }
+
+        UserDataService.createUser(user)
+        .then(() => this.props.history.push('/SignIn'))
+    }
+
+    render() {
+        let {id, firstName, lastName, userName, password} = this.state
+        return (
+            <div className="base-container">
+                <br/>
+                <div className="header">Register</div>
+                <div className="content">
+                <div className="image">
+                    <img src="https://images-na.ssl-images-amazon.com/images/I/61CfipGxS-L._AC_SL1001_.jpg" alt="new"/>
+                </div>
+                    
+                    <Formik
+                        initialValues={{id, firstName, lastName, userName, password}}
+                        onSubmit={this.onSubmit}
+                        enableReinitialize={true}
+                    >
+                        {
+                            (props) => (
+                                <Form className="form">
+                                    {/* <fieldset className="form-group">
+                                        <label >ID:</label>
+                                        <Field className="form-control" type="text" name="id" disabled />
+                                    </fieldset> */}
+                                    <fieldset className="form-group">
+                                        <label>Username</label>
+                                        <Field className="form-control" type="text" name="userName" placeholder="userName" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <label>Password</label>
+                                        <Field className="form-control" type="text" name="password" placeholder="password" />
+                                    </fieldset>
+                                    <fieldset className="form-group">
+                                        <button className="btn" type="submit">Register</button>
+                                    </fieldset>
+                                    <br/>
+                                    <br/>
+                                </Form>
+                            )
+                        }
+                    </Formik>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CreateNewUserComponent;
